8631f26867c90afc6774702a1e85d5a3c0c6aa67
jldeen <jedeen@outlook.com>
2020-11-11 11:19:38 -0800

    fix gnome bug

--- Source/Services/Tailwind.Traders.Cart.Api/routes/cartController.js
+++ Source/Services/Tailwind.Traders.Cart.Api/routes/cartController.js
@@ -21,7 +21,7 @@ class CartController {
             },
             qty: 1,
         };
-        const item = body;
+        const item = req.body;
         const doc = await this.shoppingCartDao.addItem(item);
         res.status(201).send({ message: `${doc.detailProduct.name} added to shopping cart`, id: doc.id });
         console.log(`Succsessfull added product ${doc.detailProduct.name} to shopping cart`)
